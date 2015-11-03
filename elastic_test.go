badeeffcfa5c3f6e418994aa148174dab824bfbb
wookyoung <wookay.noh@gmail.com>
2015-11-03 17:49:09 +0900

    elastic

---
+++ elastic_test.go
@@ -0,0 +1,32 @@
+package elasticcat
+
+import (
+	"elastic"
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestElastic(t *testing.T) {
+
+	errorlog := log.New(os.Stdout, "APP ", log.LstdFlags)
+
+	// Obtain a client. You can provide your own HTTP client here.
+	client, err := elastic.NewClient(elastic.SetErrorLog(errorlog))
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// Trace request and response details like this
+	//client.SetTracer(log.New(os.Stdout, "", 0))
+
+	// Ping the Elasticsearch server to get e.g. the version number
+	info, code, err := client.Ping("http://127.0.0.1:9200").Do()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	fmt.Printf("Elasticsearch returned with code %d and version %s", code, info.Version.Number)
+
+}
---
+++ elasticcat.go
@@ -0,0 +1,4 @@
+package elasticcat
+
+func elasticcat() {
+}
